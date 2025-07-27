9269cfcbef186d8272c8132f4c29b1b23db78c33
夏湾 <sunriver_kun@qq.com>
2025-07-28 00:14:43 +0800

    自动材质的build中添加Readme的拷贝

--- AutoMaterial/build.bat
+++ AutoMaterial/build.bat
@@ -1,6 +1,7 @@
 dotnet build -c Release
-cd AutoMaterial
 set OutPath=C:\Users\Administrator\Documents\Klei\OxygenNotIncluded\mods\Dev\AutoMaterial
+copy Readme.md %OutPath%
+cd AutoMaterial
 copy bin\Release\AutoMaterial.dll %OutPath%
 copy mod.yaml %OutPath%
 copy mod_info.yaml %OutPath%
